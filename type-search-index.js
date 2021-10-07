c333385c2cfba62c92dacf3350f3811cff353e0c
curtismcclelland <46636430+curtismcclelland@users.noreply.github.com>
2021-10-07 13:30:57 -0400

    Added javadoc

---
+++ type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"src.example.hello","l":"Client"},{"p":"src.example.hello","l":"Hello"},{"p":"src.example.hello","l":"Server"}];updateSearchResults();
